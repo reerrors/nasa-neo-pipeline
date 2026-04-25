79004c949dc2cf6e0f8ca97a4da6599ac001e3fa
qry <voidmartins@gmail.com>
2026-04-25 19:17:15 -0300

    transform: returning data for the 3 tables in the right format.

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,14 @@
 from extract import fetch_neo_data
-from transform import treat
+from transform import treat_neo_data
 
 raw_data = fetch_neo_data("2024-01-01","2024-01-05")
-data = treat(raw_data)
-print(data)
+
+a,c,r = treat_neo_data(raw_data,"2024-01-01","2024-01-05")
+print("Asteroids Data")
+print(f"{a}\n")
+
+print("Close Aproaches")
+print(f"{c}\n")
+
+print("Pipeline runs")
+print(f"{r}\n")
--- src/transform.py
+++ src/transform.py
@@ -1,16 +1,38 @@
 import json
+from datetime import datetime
 
-def treat(rawData):
+def treat_neo_data(rawData,startDate,endDate):
     #near_earth_objects->'2024-01-02'->[lista]
-    asteroids = rawData['near_earth_objects']['2024-01-02']
-    data = []
-    for asteroid in asteroids:
-        tmp = (asteroid['id'],
-               asteroid['name'],
-               float(asteroid['absolute_magnitude_h']),
-               asteroid['is_potentially_hazardous_asteroid']=='True',
-               float(asteroid['estimated_diameter']['kilometers']['estimated_diameter_min']),
-               float(asteroid['estimated_diameter']['kilometers']['estimated_diameter_max'])
-	)
-        data.append(tmp)
-    return data
+    asteroidData = []
+    closeAproachesData = []
+    pipelineRunsData = []
+
+    start = datetime.strptime(startDate,'%Y-%m-%d')
+    end = datetime.strptime(endDate,'%Y-%m-%d')
+
+    if not rawData:
+        pipelineRunsData.append((datetime.now(),start,end,0,"failed"))
+        return asteroidData, closeAproachesData, pipelineRunsData
+
+    sample = rawData['near_earth_objects']
+
+    for day in sample:
+        for asteroid in sample[day]:
+            tmp_ast = (asteroid['id'],
+                       asteroid['name'],
+                       float(asteroid['absolute_magnitude_h']),
+                       asteroid['is_potentially_hazardous_asteroid']=='True',
+                       float(asteroid['estimated_diameter']['kilometers']['estimated_diameter_min']),
+                       float(asteroid['estimated_diameter']['kilometers']['estimated_diameter_max'])
+	    )
+            tmp_cap = (asteroid['id'],
+                       asteroid['close_approach_data'][0]['close_approach_date'],
+                       float(asteroid['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']),
+                       float(asteroid['close_approach_data'][0]['miss_distance']['kilometers']),
+            )
+            asteroidData.append(tmp_ast)
+            closeAproachesData.append(tmp_cap)
+    tmp_run = (datetime.now(),start,end, len(asteroidData),"running")
+    pipelineRunsData.append(tmp_run)
+
+    return asteroidData, closeAproachesData, pipelineRunsData
